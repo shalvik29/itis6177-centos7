3bde3d3ac0b1e8e967354b90b7c2e714b79be075
Shalvik Shah <shahshalvik29@gmail.com>
2020-03-11 20:12:11 -0400

    change server.js

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express')
-const cors = require('cors')
+const axios = require('axios');
 const app = express()
 const port = 3000
+const cors = require('cors')
 
 app.use(cors());
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get("/", (req, res) => {
+  axios.get('https://28180ghydc.execute-api.us-east-2.amazonaws.com/default/Project3')
+    .then(response => {
+res.send("server date and time " + response.data.date + " " +response.data.time);
+    });
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
